31b0315d18bee78a0a022283ba8073e822744a93
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-05-06 19:35:54 -0400

    added the start button fucntion.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,10 @@ var gameActive = 'false';
 //
 var questionHolder = randQset.question;
 var answerHolder = randQset.answer;
-var mock1 = randQset.fakeAns1;
-var mock2 = randQset.fakeAns2;
-var mock3 = randQset.fakeAns3;
+var mock1 = randQset.answerList.a;
+var mock2 = randQset.answerList.b;
+var mock3 = randQset.answerList.c;
+var mock4 = randQset.answerList.d;
 //
 var score = 0;
 var correct = 0;
@@ -19,18 +20,14 @@ var incorrect = 0;
 
 $( document ).ready(function() {
   console.log( "ready!" );
-  $('#game').on("click", function(){
+  $('#btnStart').on("click", function(){
     gameActive = true;
+
     setTimeout(() => {
       start();
     }, 500);
     //
-
   });
-
-
-
-
 
 
 });
@@ -38,16 +35,17 @@ $( document ).ready(function() {
 function start(){
  // will create the start buttong and do a condition go to to other function if pressed
  console.log('game started');
-//
-var startBtn = $("<button>");
-startBtn.addClass("btnColor");
-startBtn.attr("onclick", genQuestion());
-startBtn.text('Start');
-$("#starter").append(startBtn);
+ $('#start-button').remove();
+genQuestion();
 //
 }
 //
 function genQuestion(){
+$('#question').text(questionHolder);
+$('#answer1').text(mock1);
+$('#answer2').text(mock2);
+$('#answer3').text(mock3);
+$('#answer4').text(mock4);
 
   console.log(questionHolder);
   console.log(answerHolder);
