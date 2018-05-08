d8efb33e01b4dd3011226f1eab1dfb9e421ad69a
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-05-07 23:33:04 -0400

    added questions and answer checker

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,37 +24,70 @@ $( document ).ready(function() {
   $('#game').hide();
   $('#btnStart').on("click", function(){
     gameActive = true;
-
     setTimeout(() => {
       start();
     }, 500);
     //
   });
-
-
 });
 //
 function start(){
  // will create the start buttong and do a condition go to to other function if pressed
- $('#game').show();
- console.log('game started');
- $('#start-button').remove();
+  $('#game').show();
+  console.log('game started');
+  $('#start-button').remove();
 genQuestion();
 //
 }
 //
 function genQuestion(){
-$('#question').text(questionHolder);
-$('#answer1').text(mock1);
-$('#answer2').text(mock2);
-$('#answer3').text(mock3);
-$('#answer4').text(mock4);
-
-  console.log(questionHolder);
-  console.log(answerHolder);
+  $('#question').text(questionHolder);
+  $('#answer1').text(mock1);
+  $('#answer2').text(mock2);
+  $('#answer3').text(mock3);
+  $('#answer4').text(mock4);
+  //
+    console.log(questionHolder);
+    checker();
+    // console.log(answerHolder);
+}
+//
+function checker() {
+  $('#answer1').on("click", function(){
+    console.log(mock1);
+    if (mock1 === answerHolder) {
+      console.log('this is the answer ' + answerHolder);
+    } else {
+      console.log('this is not the answer');
+    }
+  });
+  $('#answer2').on("click", function(){
+    console.log(mock2);
+    if (mock2 === answerHolder) {
+      console.log('this is the answer ' + answerHolder);
+    } else {
+      console.log('this is not the answer');
+    }
+  });
+  $('#answer3').on("click", function(){
+    console.log(mock3);
+    if (mock3 === answerHolder) {
+      console.log('this is the answer ' + answerHolder);
+    } else {
+      console.log('this is not the answer');
+    }
+  });
+  $('#answer4').on("click", function(){
+    console.log(mock4);
+    if (mock4 === answerHolder) {
+      console.log('this is the answer ' + answerHolder);
+    } else {
+      console.log('this is not the answer');
+    }
+  });
 
 }
 //
-function updateMe(){
-  $('#').text();
-}
+// function updateMe(){
+//   $('#').text();
+// }
--- assets/javascript/qNa.js
+++ assets/javascript/qNa.js
@@ -2,46 +2,112 @@
 var questions = [
   {
     qid : 1,
-    question: 'What is a mock question?' ,
+    question: 'Mario Kart is a video game series publish by which company?' ,
     answerList: {
-      a: 'Mock answer 1 ',
-      b: 'Mock answer 2 ',
-      c: 'Mock answer 3 ',
-      d: 'Mock answer 4 '
+      a: 'Electronic Arts',
+      b: 'Nintendo',
+      c: 'Sony',
+      d: 'Activision Blizzard'
     },
-    answer:'Mock answer 3 '
+    answer:'Nintendo'
   },
   {
-    qid : 1,
-    question: 'What is a mock question?' ,
+    qid : 2,
+    question: 'If a liquor is 100 proof how much alcohol does it contain by percentage?',
     answerList: {
-      a: 'Mock answer 1 ',
-      b: 'Mock answer 2 ',
-      c: 'Mock answer 3 ',
-      d: 'Mock answer 4 '
+      a: '100 percent',
+      b: '70 percent',
+      c: '30 percent',
+      d: '50 percent'
     },
-    answer:'Mock answer 1 '
+    answer:'50 percent'
   },
   {
-    qid : 1,
-    question: 'What is a mock question?' ,
+    qid : 3,
+    question: 'In the poker game Texas Hold’em, how many cards are dealt to each player?' ,
     answerList: {
-      a: 'Mock answer 1 ',
-      b: 'Mock answer 2 ',
-      c: 'Mock answer 3 ',
-      d: 'Mock answer 4 '
+      a: 'One',
+      b: 'Seven',
+      c: 'Two',
+      d: 'Four'
     },
-    answer:'Mock answer 4 '
+    answer:'Two'
   },
   {
-    qid : 1,
-    question: 'What is a mock question?' ,
+    qid : 4,
+    question: 'What international event for athletes with disabilities is held in the same years and cities as the Olympic Games?' ,
     answerList: {
-      a: 'Mock answer 1 ',
-      b: 'Mock answer 2 ',
-      c: 'Mock answer 3 ',
-      d: 'Mock answer 4 '
+      a: 'America\'s Cup',
+      b: 'The Paralympic Games',
+      c: 'Pan American Games',
+      d: 'World Cup'
     },
-    answer:'Mock answer 2 '
+    answer:'The Paralympic Games'
+  },
+  {
+    qid : 5,
+    question: 'What Greek mathematician is considered the founder and father of Geometry?' ,
+    answerList: {
+      a: 'Euclid',
+      b: 'The Paralympic Games',
+      c: 'Pan American Games',
+      d: 'World Cup'
+    },
+    answer:'Euclid'
+  },
+  {
+    qid : 6,
+    question: 'What is the name of the dog from the 1960s television cartoon The Jetsons?' ,
+    answerList: {
+      a: 'America\'s Cup',
+      b: 'The Paralympic Games',
+      c: 'Astro',
+      d: 'World Cup'
+    },
+    answer:'Astro'
+  },
+  {
+    qid : 7,
+    question: 'What is the heaviest naturally occurring element found on Earth?' ,
+    answerList: {
+      a: 'America\'s Cup',
+      b: 'The Paralympic Games',
+      c: 'Pan American Games',
+      d: 'Uranium (U92)'
+    },
+    answer:'Uranium (U92)'
+  },
+  {
+    qid : 8,
+    question: 'Which book holds the record of being the most stolen book from public libraries?' ,
+    answerList: {
+      a: 'America\'s Cup',
+      b: 'Guinness Book of World Records',
+      c: 'Pan American Games',
+      d: 'World Cup'
+    },
+    answer:'Guinness Book of World Records'
+  },
+  {
+    qid : 9,
+    question: 'What is the closest star to our own sun?' ,
+    answerList: {
+      a: 'Proxima Centauri',
+      b: 'The Paralympic Games',
+      c: 'Pan American Games',
+      d: 'World Cup'
+    },
+    answer:'Proxima Centauri'
+  },
+  {
+    qid : 10,
+    question: 'What is the name for the longest side of a right triangle?' ,
+    answerList: {
+      a: 'America\'s Cup',
+      b: 'The Paralympic Games',
+      c: 'Hypotenuse',
+      d: 'World Cup'
+    },
+    answer:'Hypotenuse'
   },
 ]
